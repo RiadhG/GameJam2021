ecaaf119218818e14023ffae8b4af24d9c507938
EliasElFallah <71349889+EliasElFallah@users.noreply.github.com>
2021-01-30 02:35:15 +0100

    Basic movements and firing

---
+++ New Unity Project/Assets/Script/Gunnygun.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunnygun : MonoBehaviour
+{
+    public float fireRate = 1;
+    public float damage = 10;
+    public LayerMask onlyHit;
+    public GameObject bullet;
+
+    float timeToFire = 0;
+    Transform firePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firePoint = transform.Find("");
+        if (firePoint == null)
+        {
+            Debug.Log("You dun goofed up");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fireRate == 0) 
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (Input.GetButton("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1/fireRate;
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 
+                                             Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2 (firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, onlyHit);
+        Debug.DrawLine (firePointPosition, (mousePosition - firePointPosition) * 100, Color.red);
+        GameObject currentBullet = Instantiate(bullet, new Vector3 (firePointPosition.x, firePointPosition.y, 0), Quaternion.identity);
+        currentBullet.GetComponent<Rigidbody2D>().AddForce((mousePosition - firePointPosition) * 100);
+        Destroy(currentBullet, 0.5f);
+    }
+}
---
+++ New Unity Project/Assets/Script/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;                //Floating point variable to store the player's movement speed.
+
+    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    private bool isGrounded;
+    public GameObject sword;
+    public GameObject gun;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D> ();
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        rb2d.velocity = new Vector2 (moveHorizontal * speed, rb2d.velocity[1]);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+            rb2d.velocity = new Vector2 (moveHorizontal * speed, 8);
+        if (Input.GetKeyDown(KeyCode.R))
+            rb2d.position = rb2d.position + new Vector2 (moveHorizontal * 2, 0);
+        if (Input.GetKeyDown(KeyCode.S))
+            sword.SetActive(!sword.activeSelf);
+        if (Input.GetKeyDown(KeyCode.G))
+            gun.SetActive(!gun.activeSelf);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        isGrounded = false;
+    }
+}
+
